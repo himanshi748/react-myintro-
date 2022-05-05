cab29ef1a5696f4ecad65b2ccb03b9fca695355d
Himanshi Satpute <57482731+himanshi748@users.noreply.github.com>
2022-05-05 16:51:42 +0530

    day3

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from "./index.css";
+
+const root =ReactDOM.createRoot(document.getElementsByClassName('root')[0]);
+root.render(
+  <div>
+  <i>
+  <h3>HI THERE !</h3>
+  <h1>I'M A WEB DEVELOPER </h1>
+  <h2>About Me</h2>
+  <p>I am Himanshi Satpute from bhopal.I am front-end developer focused on crafting clean & user-friendly experiences,I am passionate about building excellent web applications.I have done my Graduation from Technocrats Institute of Technology,Bhopal and i have scored 8.56 cgpa in last sem
+    along with my degree currently,I am taking a full Stack Developer Course.
+    this course help me to develop my skills.
+  </p>
+  
+
+  <h2>My Skills</h2>
+  <ul>
+    <li>HTML</li>
+    <li>CSS</li>
+    <li>Java Script</li>
+    <li>React js</li>
+  </ul>
+
+  <h2>Projects</h2>
+  <ul>
+    <li>Backtracking (HTML)</li>
+    <li>TO DO List Mobile (HTML & CSS)</li>
+    <li>TO DO List Web(HTML & CSS)</li>
+    <li>Digital clock (HTML & CSS)</li>
+    <li>TO DO List (HTML & CSS & js)</li>
+  </ul>  
+
+<h2>Contact Me</h2>
+  <ul>
+    <li>phone : +91 96xyz</li>
+    <li>Email : xyz@gmail.com</li>
+    <li>Bhopal,MP</li>
+    
+  </ul>
+
+
+  </i>
+  </div>
+  );
